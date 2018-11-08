c3db8f0127850512afa4f057c3f645e66de78188
Sajad Hashemian <sajadhsm@gmail.com>
2018-11-08 13:48:55 +0330

    Implement logout operation

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,6 +5,12 @@
     <v-toolbar app clipped-right dark>
       <v-toolbar-side-icon left></v-toolbar-side-icon>
       <v-toolbar-title>UIAI2018</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon @click="logout">
+        <v-icon>power_settings_new</v-icon>
+      </v-btn>
     </v-toolbar>
 
     <v-content>
@@ -19,6 +25,14 @@ import Sidebar from "../components/Sidebar";
 export default {
   components: {
     Sidebar
+  },
+  methods: {
+    logout() {
+      localStorage.removeItem("access");
+      this.$store.commit("SET_IS_LOGGED_IN", false);
+      this.$store.commit("SET_ACCESS_TOKEN", "");
+      this.$router.push({ name: "login" });
+    }
   }
 };
 </script>
