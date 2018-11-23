3d6c4d6c0e6845cdd71b8deadeb14e7b00ff6ecb
Sajad Hashemian <sajadhsm@gmail.com>
2018-11-23 16:23:04 +0330

    Define faDate filter

--- src/main.js
+++ src/main.js
@@ -10,6 +10,17 @@ axios.defaults.baseURL = process.env.VUE_APP_ROOT_API;
 
 Vue.config.productionTip = false
 
+Vue.filter('faDate', value => {
+  if (!value) return '-'
+  return new Date(value).toLocaleString('fa', {
+    year: '2-digit',
+    month: 'short',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric'
+  });
+})
+
 new Vue({
   router,
   store,
